e2405eff1af114ad409f1e0d160866b475d89786
Daryl DeFord <daryl.r.deford@gmail.com>
2023-01-17 11:55:19 -0800

    Add files via upload

---
+++ Figures/analysis_of_probabilistic_approach_uniform_sampling.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import scipy.stats as st
+
+for i in range(7,20):
+	df = pd.read_csv(f'./uniform_comparison_{i}.csv',header=None)
+	k = int(np.floor( (i-2)/2))
+	
+	temp = [[] for x in range(k)]
+	for ind, row in df.iterrows():
+		for x in range(k):
+			temp[x].append(eval(row[x])[-1])
+
+	print(i,[len(temp[y]) for y in range(k)])
+	print(i, 'Mean', [np.mean(temp[y]) for y in range(k)])
+	print(i,'Standard Deviation', [np.std(temp[y]) for y in range(k)])
+	print(i,'Skewness',[st.skew(temp[y]) for y in range(k)])
+    
+    
+		
+
